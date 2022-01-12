ff85445f9fffc796fb025b9afdd0a46d868f0b22
Nedim Begić <hi@residence-of-code.com>
2022-01-12 15:59:45 +0100

    Create functionmodifiers.sol

---
+++ functionmodifiers.sol
@@ -0,0 +1,18 @@
+contract owned {
+  function owned() { owner = msg.sender; }
+  address owner;
+
+  // This contract only defines a modifier but does not use it - it will
+  // be used in derived contracts.
+  // The function body is inserted where the special symbol "_" in the
+  // definition of a modifier appears.
+  modifier onlyowner { if (msg.sender == owner) _ }
+}
+contract mortal is owned {
+  // This contract inherits the "onlyowner"-modifier from "owned" and
+  // applies it to the "kill"-function, which causes that calls to "kill"
+  // only have an effect if they are made by the stored owner.
+  function kill() onlyowner {
+    suicide(owner);
+  }
+}
